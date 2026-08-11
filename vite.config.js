dfd343ae157faef5251926874f2ef1651842b3cb
bonzo-coder <113475630+bonzo-coder@users.noreply.github.com>
2026-08-11 20:02:06 +0200

    Create deploy-pages branch and capture current site updates

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import svgr from 'vite-plugin-svgr' 
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/eeettteess/',
+  base: "/",
   plugins: [
-    react(),
-    svgr({
+    react(), 
+    svgr({ 
       svgrOptions: {
         // svgr options
       },
     }),
-  ],
+  ], 
 })
